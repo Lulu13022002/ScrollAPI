34f4833632f1627a7b51b80ac183a4981b911649
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2018-09-01 21:43:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,14 @@ function isPhone() {
 }*/
 
 function storageAvailable(type) {
-	try {
-		var storage = window[type],
-			x = '__storage_test__';
-		storage.setItem(x, x);
-		storage.removeItem(x);
-		return true;
-	}
-	catch(e) {
-		return false;
-	}
+  try {
+    var storage = window[type], x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch(e) {
+    return false;
+  }
 }
 
 function isMobile() {

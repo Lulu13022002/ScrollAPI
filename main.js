a2c9c707797977be367880ab769ba653fc8812bc
Lulu13022002 <41980282+Lulu13022002@users.noreply.github.com>
2018-10-25 20:50:19 +0200

    Fix check of errors of configuration object

--- main.js
+++ main.js
@@ -98,11 +98,14 @@ var scrollAPI = (function() {
     opt = opt || {};
     if (!api.compatibility()) throw new EvalError('[ScrollAPI] Please update your navigator');
     api.config(opt);
-    if(!api.isInDOM(config.target)) throw new TypeError('[ScrollAPI] config.target must be an htmlelement');
-    if(!parseInt(config.scroll) && config.scroll !== undefined) throw new TypeError('[ScrollAPI] config.scroll must be a number');
+    var Pscroll = parseInt(config.scroll);
+    if("scroll" in config && !Pscroll){
+      if(Pscroll === 0) throw new TypeError('[ScrollAPI] config.scroll is useless');
+      else throw new TypeError('[ScrollAPI] config.scroll must be a number');
+    }
     window.addEventListener("load", function() {
       setTimeout(function() {
-        scrollAPI.scrollTo(config.target, parseInt(config.scroll));
+        scrollAPI.scrollTo(config.target, Pscroll);
       });
     });
     api.addEventListener("scroll", function(e) {
